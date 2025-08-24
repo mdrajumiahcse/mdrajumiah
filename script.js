7103166d40250fa7dbf37d073b76cd0f7f7cf1fc
mdrajumiahcse <mdrajumiahcse@gmail.com>
2025-08-25 00:43:52 +0600

    update index, script and styele

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
+//amimaiton ar jonoo js
+    
+    const typingElement = document.querySelector(".typing");
+const words = ["Stack Developer", "Specialist", "Engineer"];
+let wordIndex = 0;
+let charIndex = 0;
+let currentWord = "";
+let isDeleting = false;
+
+function typeEffect() {
+  currentWord = words[wordIndex];
+  let displayedText = currentWord.substring(0, charIndex);
+  typingElement.textContent = displayedText + "|";
+
+  if (!isDeleting) {
+    charIndex++;
+    if (charIndex > currentWord.length) {
+      isDeleting = true;
+      setTimeout(typeEffect, 1200);
+      return;
+    }
+  } else {
+    charIndex--;
+    if (charIndex < 0) {
+      isDeleting = false;
+      wordIndex = (wordIndex + 1) % words.length;
+    }
+  }
+
+  setTimeout(typeEffect, isDeleting ? 80 : 120);
+}
+
+typeEffect();
+
+
+
+
+// Counter Animation
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+  counter.innerText = "0";
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const current = +counter.innerText;
+    const increment = target / 100; // speed control
+
+    if (current < target) {
+      counter.innerText = `${Math.ceil(current + increment)}`;
+      setTimeout(updateCounter, 30);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCounter();
+});
+
+
+
+//phone view ar responsvie ar joono
 const menuBtn = document.querySelector('.menu-btn');
     const mobileNav = document.querySelector('.mobile-nav');
     const overlay = document.querySelector('.overlay');
